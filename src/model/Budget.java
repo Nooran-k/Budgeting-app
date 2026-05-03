a5ea4521a297c9f7760745c7e0cc0f6eec1beff5
OmarAhmedd88 <omarboshksh88@gmail.com>
2026-05-03 21:51:53 +0300

    Add files via upload

---
+++ src/model/Budget.java
@@ -0,0 +1,46 @@
+package model;
+
+public class Budget {
+    private String categoryName; 
+    private double budgetamount;   
+    private double spentAmount;   
+    private int alertThreshold; 
+        private BudgetStatus status;  
+  
+    
+ 
+    public Budget(String categoryname , double budgetamount, int alertThreshold ){
+          this.categoryName = categoryname;
+          this.budgetamount = budgetamount;
+          this.alertThreshold = alertThreshold;
+          this.spentAmount = 0;
+          this.status = BudgetStatus.ON_TRACK;
+    }
+    public double calc_percentage (){
+        return (spentAmount/budgetamount) * 100;
+    }
+    public BudgetStatus calc_status(){
+          double percent = calc_percentage();
+          if (percent >= 100) { return BudgetStatus.EXCEEDED; }
+          if (percent >= alertThreshold) { return BudgetStatus.NEAR_LIMIT; }
+          return BudgetStatus.ON_TRACK; 
+    }
+    public void add_expense(double amount){
+        this.spentAmount += amount;
+        this.status = calc_status();
+    }
+    public double calculateRemaining() { 
+    return budgetamount - spentAmount; 
+}
+    public void setCategory(String category) { this.categoryName = category; }
+public void setBudgetAmount(double budgetamount) { this.budgetamount = budgetamount; }
+public void setSpentAmount(double spentAmount) { this.spentAmount = spentAmount; }
+public void setAlertThreshold(int alertThreshold) { this.alertThreshold = alertThreshold; }
+    
+public String getCategory() { return categoryName; }
+  public int getAlertThreshold() { return alertThreshold; }
+    public double getBudgetAmount() { return budgetamount; }
+    public double getSpentAmount() { return spentAmount; }
+
+}
+ 
