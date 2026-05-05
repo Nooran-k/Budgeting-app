f10877adb5ec7f903bd198e3ab0e9495f7c778c5
mohisham06 <mooodyhisham2006@gmail.com>
2026-05-06 01:51:53 +0300

    Initialize Budget App with main frame and controllers

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,32 @@
 import ui.MainFrame;
+import data.DataStore;
+import controller.TransactionController;
+import view.AddTransactionView;
+import controller.NotificationController;
+
+
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
         new MainFrame();
+        NotificationController notifController  = new NotificationController();
+        TransactionController  txController     = new TransactionController(budgetController);
+         SwingUtilities.invokeLater(() -> {
+
+            JFrame frame = new JFrame("Budget App");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(750, 600);
+            frame.setLocationRelativeTo(null);
+
+            JTabbedPane tabs = new JTabbedPane();
+            tabs.addTab("Transactions",
+                    new AddTransactionView(txController, notifController));
+    
+
+            frame.add(tabs);
+            frame.setVisible(true);
+        });
     }
 
 }
