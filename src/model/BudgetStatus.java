d68d90a9f435f9fd1dcf46d6de66d1d0421e466f
OmarAhmedd88 <omarboshksh88@gmail.com>
2026-05-07 21:13:59 +0300

    Update BudgetStatus.java

--- src/model/BudgetStatus.java
+++ src/model/BudgetStatus.java
@@ -1,7 +1,26 @@
 package model;
 
+/**
+ * Represents the possible states of a Budget.
+ * 
+ * This enum is used to determine the current condition of a budget
+ * based on spending compared to the defined limit.
+ * 
+ * It is also used in the UI to control visual indicators such as colors:
+ * <ul>
+ *   <li>ON_TRACK   → Budget usage is within safe limits (green)</li>
+ *   <li>NEAR_LIMIT → Spending has reached the alert threshold (orange)</li>
+ *   <li>EXCEEDED   → Spending has exceeded the budget limit (red)</li>
+ * </ul>
+ */
 public enum BudgetStatus {
+
+    /** Budget usage is within acceptable range */
     ON_TRACK,
+
+    /** Budget usage has reached the alert threshold */
     NEAR_LIMIT,
+
+    /** Budget limit has been exceeded */
     EXCEEDED
 }
